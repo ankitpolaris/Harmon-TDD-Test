f15a36bf4eb406ac6064e40296a6cd6ae3e4ff2a
Ankit Khanna <ankit@deskfactors.com>
2022-06-13 17:02:23 +0530

    Created class and the structure

--- Harmon-TDD-Test/ViewController.swift
+++ Harmon-TDD-Test/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let stringCalculator = StringCalculator()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        stringCalculator.add(numbers: "")
+        
     }
 
+    
+    
 
+}
+
+
+class StringCalculator {
+    
+    func add (numbers: String) -> Int {
+        var sum = 0
+        
+        if numbers == "" { return 0 }
+         
+        
+        
+        return sum
+        
+    }
 }
 
